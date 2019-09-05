bcf4530effa5906363aef15992673d99b55a0966
rustylocks79 <jeremydellock570@gmail.com>
2019-09-05 15:45:41 -0400

    Generate Random Password.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Random;
+
 public class Main {
+    private static int MAX_PASSWORD_LENGTH = 20;
+    private static int MIN_PASSWORD_LENGTH = 6;
+
+    public static String generateRandomPassword() {
+        Random random = new Random();
+        int length = random.nextInt(MAX_PASSWORD_LENGTH) + MIN_PASSWORD_LENGTH;
+        char[] chars = new char[length];
+        for(int i = 0; i < length; i++) {
+            chars[i] = (char) (random.nextInt(89) + 33);
+        }
+        return new String(chars);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World! ");
+        System.out.println(generateRandomPassword());
     }
 }
