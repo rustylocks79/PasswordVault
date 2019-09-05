40064b3807507fb32f3c1c1189295a91bd051442
bdwarner99 <54958683+bdwarner99@users.noreply.github.com>
2019-09-05 15:30:41 -0400

    Testing Contributers

---
+++ src/Account.java
@@ -0,0 +1,34 @@
+public class Account {
+
+    private String id,username,password;
+
+    public Account(String id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
