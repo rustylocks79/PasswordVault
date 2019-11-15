6d262939cc0759dadac9638c4e5c731dad7b8ffa
bdwarner99 <54958683+bdwarner99@users.noreply.github.com>
2019-11-15 11:43:33 -0500

    Refactored main with new backend

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -30,156 +31,189 @@ public class Main {
         return validated;
     }
 
-    // TODO reset options menu
-    public static void resetMasterPassword(User user, Scanner scanner) {
+    public static boolean checkInput(char[] input) {
+        for(char c:input) {
+            if(c == ';') return false;
+        }
+        return true;
+    }
 
-        System.out.println("Are you sure? The process cannot be reversed");
+    public static boolean checkMasterPassword(User user) {
+        boolean passwordloop = true;
+        Console console =  System.console();
+        boolean passwordverified = false;
+
+        while (passwordloop) {
+            System.out.print("Please enter password: ");
+            char[] checkpassword = console.readPassword();
+
+            //TODO fix after password verification is added
+//            if (checkpassword.equals(user.getMasterPassword())) {
+//                System.out.println("\nPASSWORD VERIFIED\nUSER AUTHENTICATED\nREMOVING SECURITY RESTRICTIONS");
+//                Arrays.fill(checkpassword, '0');
+//                passwordverified = true;
+//                passwordloop = false;
+//            } else {
+//                System.out.println("\nPassword incorrect, please try again\n");
+//            }
+
+        }
+        return passwordverified;
+    }
+
+    // TODO reset options menu
+    public static void resetMasterPassword(User user, BufferedReader reader) throws IOException {
+
+        System.out.println("Are you sure? The process cannot be reverted");
         int result2 = InputHelper.optionMenu("Yes", "No");
 
         if (result2 == 0) {
 
             boolean methodloop = true;
+            Console console =  System.console();
 
             while (methodloop) {
                 System.out.print("Please input new master password: ");
-                String masterpassword = scanner.nextLine();
+                char[] masterpassword = console.readPassword();
 
                 if (checkInput(masterpassword)) {
-//                    user.setMasterPassword(masterpassword);
+                    //TODO fix after reset function added
+                    //user.resetMastPassword(masterpassword);
+                    Arrays.fill(masterpassword,'0');
                     methodloop = false;
                 } else {
                     System.out.println("Input cannot contain commas or semicolons, please try again");
                     System.out.print("Press enter to continue");
-                    scanner.nextLine();
+                    reader.readLine();
                 }
             }
 
         } else {
             System.out.println("Very well, process cancelled");
             System.out.print("Press enter to continue");
-            scanner.nextLine();
+            reader.readLine();
         }
 
     }
 
     // TODO add options menu
-    public static void addAccount(User user, Scanner scanner) {
+    public static void addAccount(User user, BufferedReader reader) {
 
-        String acctID = "";
-        String acctUser = "";
-        String acctPass = "";
-
-        boolean idLoop = true;
-        while (idLoop) {
-            System.out.print("Please enter account ID: ");
-            String newID = scanner.nextLine();
-            if (!checkInput(newID)) {
-                System.out.println("Input cannot contain commas or semicolons, please try again");
-                System.out.print("Press enter to continue");
-                scanner.nextLine();
-            } else {
-                idLoop = false;
-                acctID = newID;
-            }
-        }
-
-        boolean userLoop = true;
-        while (userLoop) {
-            System.out.print("Please enter account Username: ");
-            String newUsername = scanner.nextLine();
-            if (!checkInput(newUsername)) {
-                System.out.println("Input cannot contain commas or semicolons, please try again");
-                System.out.print("Press enter to continue");
-                scanner.nextLine();
-            } else {
-                userLoop = false;
-                acctUser = newUsername;
-            }
-        }
-
-        int resultpass = InputHelper.optionMenu("Generate Random Password", "Input Custom Password");
-        String newPassword;
-
-        if (resultpass == 0) {
-            // TODO refactor use of generated password
-            //newPassword = generateRandomPassword();
-            //System.out.println("Password generated: " + newPassword + "\n");
-            //acctPass = newPassword;
-        } else {
-            boolean passLoop = true;
-            while (passLoop) {
-                System.out.print("Please enter account Password: ");
-                newPassword = scanner.nextLine();
-                if (!checkInput(newPassword)) {
-                    System.out.println("Input cannot contain commas or semicolons, please try again");
-                    System.out.print("Press enter to continue");
-                    scanner.nextLine();
-                } else {
-                    passLoop = false;
-                    acctPass = newPassword;
-                }
-            }
-        }
-
+//        String acctID = "";
+//        String acctUser = "";
+//        String acctPass = "";
+//
+//        boolean idLoop = true;
+//        while (idLoop) {
+//            System.out.print("Please enter account ID: ");
+//            String newID = scanner.nextLine();
+//            if (!checkInput(newID)) {
+//                System.out.println("Input cannot contain commas or semicolons, please try again");
+//                System.out.print("Press enter to continue");
+//                scanner.nextLine();
+//            } else {
+//                idLoop = false;
+//                acctID = newID;
+//            }
+//        }
+//
+//        boolean userLoop = true;
+//        while (userLoop) {
+//            System.out.print("Please enter account Username: ");
+//            String newUsername = scanner.nextLine();
+//            if (!checkInput(newUsername)) {
+//                System.out.println("Input cannot contain commas or semicolons, please try again");
+//                System.out.print("Press enter to continue");
+//                scanner.nextLine();
+//            } else {
+//                userLoop = false;
+//                acctUser = newUsername;
+//            }
+//        }
+//
+//        int resultpass = InputHelper.optionMenu("Generate Random Password", "Input Custom Password");
+//        String newPassword;
+//
+//        if (resultpass == 0) {
+//            // TODO refactor use of generated password
+//            //newPassword = generateRandomPassword();
+//            //System.out.println("Password generated: " + newPassword + "\n");
+//            //acctPass = newPassword;
+//        } else {
+//            boolean passLoop = true;
+//            while (passLoop) {
+//                System.out.print("Please enter account Password: ");
+//                newPassword = scanner.nextLine();
+//                if (!checkInput(newPassword)) {
+//                    System.out.println("Input cannot contain commas or semicolons, please try again");
+//                    System.out.print("Press enter to continue");
+//                    scanner.nextLine();
+//                } else {
+//                    passLoop = false;
+//                    acctPass = newPassword;
+//                }
+//            }
+//        }
+//
 //        Account newAccount = new Account(acctID, acctUser, acctPass);
 //        user.getAccounts().add(newAccount);
-
-        System.out.println("Account added");
-        System.out.print("Press enter to continue");
-        String proceed = scanner.nextLine();
+//
+//        System.out.println("Account added");
+//        System.out.print("Press enter to continue");
+//        String proceed = scanner.nextLine();
 
     }
 
     // TODO retrieve options menu
-    public static void retrieveAccount(User user, Scanner scanner) {
+    public static void retrieveAccount(User user, BufferedReader reader) {
 
-        System.out.print("Please enter the ID of the account you wish to view: ");
-        String retrieveID = scanner.nextLine();
-        Account targetAccount = null;
-
+//        System.out.print("Please enter the ID of the account you wish to view: ");
+//        String retrieveID = scanner.nextLine();
+//        Account targetAccount = null;
+//
 //        for (Account acc : user.getAccounts()) {
 //            if (acc.getId().equals(retrieveID)) {
 //                targetAccount = acc;
 //            }
 //        }
-
-        if (targetAccount == null) {
-            System.out.println("Sorry, that account does not exist, please try again later");
-            System.out.print("Press enter to continue");
-            String proceed = scanner.nextLine();
-        } else {
-            System.out.println("Account Username: " + targetAccount.getUsername());
-            System.out.println("Account Password: " + targetAccount.getPassword());
-            System.out.print("Press enter to continue");
-            String proceed = scanner.nextLine();
-        }
+//
+//        if (targetAccount == null) {
+//            System.out.println("Sorry, that account does not exist, please try again later");
+//            System.out.print("Press enter to continue");
+//            String proceed = scanner.nextLine();
+//        } else {
+//            System.out.println("Account Username: " + targetAccount.getUsername());
+//            System.out.println("Account Password: " + targetAccount.getPassword());
+//            System.out.print("Press enter to continue");
+//            String proceed = scanner.nextLine();
+//        }
 
     }
 
     // TODO sharing options menu
-    public static void shareAccount(User user, Scanner scanner) throws NoSuchPaddingException, NoSuchAlgorithmException, IOException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException {
+    public static void shareAccount(User user, BufferedReader reader) throws NoSuchPaddingException, NoSuchAlgorithmException, IOException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException {
 
-        System.out.print("Please enter the ID of the account you wish to share: ");
-        String shareID = scanner.nextLine();
-
-        Account targetAccount = null;
-
+//        System.out.print("Please enter the ID of the account you wish to share: ");
+//        String shareID = scanner.nextLine();
+//
+//        Account targetAccount = null;
+//
 //        for (Account acc : user.getAccounts()) {
 //            if (acc.getId().equals(shareID)) {
 //                targetAccount = acc;
 //            }
 //        }
-
-        if (targetAccount == null) {
-            System.out.println("Sorry, that account does not exist, please try again later");
-            System.out.print("Press enter to continue");
-            String proceed = scanner.nextLine();
-        } else {
+//
+//        if (targetAccount == null) {
+//            System.out.println("Sorry, that account does not exist, please try again later");
+//            System.out.print("Press enter to continue");
+//            String proceed = scanner.nextLine();
+//        } else {
 //            targetAccount.saveToFile("share.txt");
-            System.out.println("File created in local directory: share.txt");
-            System.out.print("Press enter to continue");
-            String proceed = scanner.nextLine();
-        }
+//            System.out.println("File created in local directory: share.txt");
+//            System.out.print("Press enter to continue");
+//            String proceed = scanner.nextLine();
+//        }
 
     }
 
@@ -192,57 +226,46 @@ public class Main {
 //            char[] arr1 = new char[10];
 //
 //            System.out.println("Input up to 10 characters: ");
-//            reader.read(arr1, 0, 10);
+//            reader.read(arr1, 0, 10); //arr1 => target array, 0 => starting index, 10 => max characters
 //
 //            System.out.println(arr1);
         }
 
-        // TODO fix this main method stuff
-        int result = -1;
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        // TODO creating a user and retrieving file
         User user = null;
-        File file = new File("save.txt");
+//        File file = new File("save.txt");
 //        if (file.exists()) {
 //            user = new User(file);
 //        } else {
 //            user = new User("password");
 //        }
-//
-//        boolean passwordloop = true;
-//
-//        while (passwordloop) {
-//            System.out.print("Please enter password: ");
-//            String checkpassword = scanner.nextLine();
-//
-//            if (checkpassword.equals(user.getMasterPassword())) {
-//                System.out.println("\nPASSWORD VERIFIED\nUSER AUTHENTICATED\nREMOVING SECURITY RESTRICTIONS");
-//                passwordloop = false;
-//            } else {
-//                System.out.println("\nPassword incorrect, please try again\n");
-//            }
-//
-//        }
 
         boolean menuLoop = true;
-
         while (menuLoop) {
 
+            int result = -1;
             System.out.println("\nJEB'S PASSWORD VAULT - MAIN MENU\n");
             result = InputHelper.optionMenu("Reset Master Password", "Add Account", "Retrieve Account", "Share Account", "Exit");
 
             switch (result) {
 
                 case 0:
-                    resetMasterPassword(user, scanner);
+                    if(!checkMasterPassword(user)) {break;}
+                    resetMasterPassword(user, reader);
                     break;
                 case 1:
-                    addAccount(user, scanner);
+                    if(!checkMasterPassword(user)) {break;}
+                    addAccount(user, reader);
                     break;
                 case 2:
-                    retrieveAccount(user, scanner);
+                    if(!checkMasterPassword(user)) {break;}
+                    retrieveAccount(user, reader);
                     break;
                 case 3:
-                    shareAccount(user, scanner);
+                    if(!checkMasterPassword(user)) {break;}
+                    shareAccount(user, reader);
                     break;
                 case 4:
                     menuLoop = false;
